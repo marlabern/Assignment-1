59537c6e241d658ec614c15aed9c866dd0b48808
mabe6081@colorado.edu <MarlaBernstein@rgnt2-105-118-dhcp.int.colorado.edu>
2017-01-25 14:48:51 -0700

    Added Assignment 1

---
+++ js/portrait.js
@@ -0,0 +1,15 @@
+function write1(){
+	document.getElementById("hair").src= "img/hairtext.png";
+}
+function write2(){
+	document.getElementById("ear").src= "img/eartext.png";
+}
+function write4(){
+	document.getElementById("nose").src= "img/nosetext.png";
+}
+function write5(){
+	document.getElementById("righteye").src= "img/righteyetext.png";
+}
+function write6(){
+	document.getElementById("lips").src= "img/lipstext.png";
+}
